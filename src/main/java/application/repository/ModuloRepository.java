5159d1d2e6486cbc64d83a97e8de40d7e4fa1d64
Lucas Finoti Rodrigues <105088725+lucasfinotirodrigues@users.noreply.github.com>
2025-05-27 19:02:32 +0000

    feat: repositories adicionados

--- src/main/java/application/repository/ModuloRepository.java
+++ src/main/java/application/repository/ModuloRepository.java
@@ -1,11 +1,14 @@
 package application.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import application.model.Modulo;
 
+import java.util.List;
+
 @Repository
 public interface ModuloRepository extends JpaRepository<Modulo, Long> {
-    
+    List<Modulo> findByCursoId(Long cursoId);
 }
